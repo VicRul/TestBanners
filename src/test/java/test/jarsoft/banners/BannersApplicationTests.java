eb17580d40ab9b52b5818c66741634448fbc1810
Victor Sotskov <51897005+VicRul@users.noreply.github.com>
2020-07-21 09:50:53 +0700

    Первый коммит

---
+++ src/test/java/test/jarsoft/banners/BannersApplicationTests.java
@@ -0,0 +1,13 @@
+package test.jarsoft.banners;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BannersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
