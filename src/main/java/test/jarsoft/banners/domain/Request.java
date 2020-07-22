865aab34bb082a61238d755bee193d7a2b2ff291
Victor Sotskov <51897005+VicRul@users.noreply.github.com>
2020-07-22 15:16:36 +0700

    Скорректировал импорты после добавления формата даты

--- src/main/java/test/jarsoft/banners/domain/Request.java
+++ src/main/java/test/jarsoft/banners/domain/Request.java
@@ -13,7 +13,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
